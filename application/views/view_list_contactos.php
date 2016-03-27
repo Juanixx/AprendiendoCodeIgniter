55721b3da5970db65f358277244142b60f6aeb73
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-03-27 17:59:00 -0600

    Ahora se muestran resultados en forma de lista

--- application/views/view_list_contactos.php
+++ application/views/view_list_contactos.php
@@ -1,9 +1,15 @@
-<h1>Listado de contactos<!--<?php echo $nombre.' - '.$nombre2 ?>--></h1>
+<h1>Listado de contactos V2<!--<?php echo $nombre.' - '.$nombre2 ?>--></h1>
 </br></br>
-<?php
-	if(empty($listado)){
-		echo "Sin Contactos";
-	}else{
-		echo "Con Contactos";
-	}
-?>
+<?php	if(empty($listado)){ ?>
+		<h1><b>Sin Contactos</b></h1>
+<?php	}else{	?>
+		Tienes <?php echo count($listado)?>	Contacto (s)
+		<br/><br/>
+		<?php 
+		foreach($listado as $contacto){
+			/**Después de $contacto, se especifica que campos de la tabla 
+			se van a imprimir 
+			*/
+			echo $contacto->con_nombre.'<br/>';
+		}?>
+<?php	}	?>
